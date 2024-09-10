9006182b16f94d4146513d8cde2365766dbdf56f
jacob <jacob.myoung@2pmlab.co>
2024-09-10 21:28:41 +0900

    fix: Delete unused component

--- app/src/main/java/com/hj/runnotion/Constants.kt
+++ app/src/main/java/com/hj/runnotion/Constants.kt
@@ -1,5 +1,5 @@
 package com.hj.runnotion
 
 object Constants {
-    const val DEFAULT_URL = "https://www.notion.so/hgwt-wiki/Daily-f99691426287406c962697511259edb4"
+    const val DEFAULT_URL = "https://www.notion.so/hgwt-wiki/Developer-8f887972919442e19e2ab9e9010464ab"
 }
